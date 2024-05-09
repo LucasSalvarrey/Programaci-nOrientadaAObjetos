91ae474d5e41197c7c6828ede5b32be459eb3c85
LucasSalvarrey <lucas.salvarrey@davinci.edu.ar>
2024-05-09 19:15:16 -0300

    Primer commit

---
+++ src/ej2herencia/Main.java
@@ -0,0 +1,12 @@
+package ej2herencia;
+
+import javax.swing.JOptionPane;
+
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hola, hoy hay paro, asi que no hay clase :)");
+	}
+
+}
